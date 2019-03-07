042349e07199ef318f6241cbcdab75476215764b
pestovpvl <pestovpvl@mail.ru>
2019-03-06 21:12:02 -0500

    Find relevant string from file

---
+++ reviews.rb
@@ -0,0 +1,16 @@
+lines = []
+
+File.open("reviews.txt") do |review_file|
+    lines = review_file.readlines
+end
+
+relevant_lines = []
+
+lines.each do |line|
+    if line.include?("Truncated")
+        relevant_lines << lines
+    end
+end
+
+puts relevant_lines
+
