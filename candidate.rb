f7b7e164582615708251a4eea1696e6df1d89258
pestovpvl <pestovpvl@mail.ru>
2019-03-12 21:39:29 -0400

    Add requared argument to parameters for Initialize method

--- candidate.rb
+++ candidate.rb
@@ -1,6 +1,6 @@
 class Candidate
  attr_accessor :name, :age, :occupation, :hobby, :birthplace
- def initialize(name, options = {age: nil, occupation: nil, hobby: nil, birthplace: "Sleepy Creek"})
+ def initialize(name, age: nil, occupation: nil, hobby: nil, birthplace: "Sleepy Creek")
     self.name = name
     self.age = age
     self.occupation = occupation
