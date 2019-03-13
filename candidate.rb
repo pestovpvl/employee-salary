dfff08763f35d9c61fe69971751004ad241f6113
pestovpvl <pestovpvl@mail.ru>
2019-03-12 21:03:48 -0400

    Add blank keywords for argumets

--- candidate.rb
+++ candidate.rb
@@ -1,11 +1,11 @@
 class Candidate
  attr_accessor :name, :age, :occupation, :hobby, :birthplace
- def initialize(name, options = {})
+ def initialize(name, options = {age: nil, occupation: nil, hobby: nil, birthplace: "Sleepy Creek"})
     self.name = name
-    self.age = options[:age]
-    self.occupation = options[:occupation]
-    self.hobby = options[:hobby]
-    self.birthplace = options[:birthplace]
+    self.age = age
+    self.occupation = occupation
+    self.hobby = hobby
+    self.birthplace = birthplace
  end
 end
 
