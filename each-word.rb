211c7ffc5be2a46a2f31796f93c372b9f92ed7d0
pestovpvl <pestovpvl@mail.ru>
2019-03-23 11:54:45 -0400

    Add documentation for file each-word.rb

--- each-word.rb
+++ each-word.rb
@@ -1,6 +1,11 @@
+# This class allows you to perform various
+# operations on the words in a string.
 class WordSplitter
     include Enumerable
+    # The string to split into words.
     attr_accessor :string
+    # Passes each word in the string to a block
+    # one at a time.
     def each
         string.split(" ").each do |word|
             yield word
